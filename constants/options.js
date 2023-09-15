ab4efc53390b4e49aa07966b9539010d7894017f
austinko <skygg@naver.com>
2023-09-15 09:18:08 +0900

    도메인 묻은 값들 제거중

--- constants/options.js
+++ constants/options.js
@@ -1,21 +1,9 @@
-//아이 앨범 ui 혹은 정책 관련 설정
+/**
+ * @description 프로젝트별 상수 표시해야 할 옵션 값
+ * @author 고종현
+ * @version 0.0.1
+ */
 export default class OPTIONS {
-	//요금제별 가용 용량
-	static MAXIMUM_STORAGE = {
-		FREE_TIER: 1 * 1024 * 1024 * 1024, //5GB
-		PREMIUM: 10 * 1024 * 1024 * 1024, //10GB
-	};
-
-	static MAXIMUM_UPLOAD_ABLE_IMAGE_COUNT = {
-		FREE_TIER: 1000,
-		PREMIUM: 1000000,
-	};
-
-	static MAXIMUM_UPLOAD_ABLE_VIDEO_COUNT = {
-		FREE_TIER: 100,
-		PREMIUM: 1000,
-	};
-
 	//슬라이드 쇼 옵션
 	static SLIDE_SHOW = {
 		NEXT_DELAY: 10000, //다음 슬라이드로 넘어가는 딜레이
@@ -32,31 +20,4 @@ export default class OPTIONS {
 		SIZE: 30, //페이지 1회 호출당 불러올 데이터 수
 		DEFAULT_PAGE: 1, //시작 페이지 넘버
 	};
-
-	static FAMILY_KRS = ['아빠', '친할머니', '친할아버지', '외할머니', '외할아버지', '친인척', '지인'];
-
-	//전체 너비
-	static MOBILE_MAX_WIDTH = 640;
-
-	static URLS = {
-		KAKAO_BANNER_IMAGE: 'https://iandna-contents.s3.ap-northeast-2.amazonaws.com/banner/album-kako-banner.png',
-		MOBILE_PLAY_STORE: 'https://play.google.com/store/apps/details?id=com.namyang.bebe',
-		WEB_PLAY_STORE: 'https://play.google.com/store/apps/details?id=com.namyang.bebe',
-		MOBILE_APP_STORE: 'https://itunes.apple.com/kr/app/id1113295590',
-		WEB_APP_STORE: 'https://itunes.apple.com/kr/app/id1113295590',
-	};
-
-	//인증 및 앨범 설정 정보가 필요없는 페이지 인지 확인
-	static IGNORED_ESSENTIAL_LOGIC_PATHS = ['/404'];
-
-	static MAXIMUM_FOLDER_STR_LENGTH = 25;
-
-	// 공개범위 설정
-	static PUBLIC_TYPE = [
-		{ key: 'PARENTS', value: 1, title: '엄마, 아빠' },
-		{ key: 'DADDY_FAMILY', value: 2, title: '친가' },
-		{ key: 'MOMMY_FAMILY', value: 3, title: '외가' },
-		{ key: 'OTHER_FAMILY', value: 4, title: '친인척' },
-		{ key: 'FRIENDS', value: 5, title: '지인' },
-	];
 }
--- constants/query.js
+++ constants/query.js
@@ -2,39 +2,7 @@ import HighFunc from '@lib/function/hof';
 import CustomError from '@lib/error';
 
 export class QueryKeys {
-	//이미지 관련
-	//DEPTH 1
-	static IMAGE_QUERY_GROUP = ['IMAGE_QUERY_GROUP']; //이미지 관련 QUERY
-	//------DEPTH 2
-	static IMAGE_DETAIL = [...QueryKeys.IMAGE_QUERY_GROUP, 'IMAGE_DETAIL']; //이미지 상세
-	static IMAGE_INFLUENCED = [...QueryKeys.IMAGE_QUERY_GROUP, 'IMAGE_INFLUENCED']; //한장의 이미지라도 변하면 영향을 받는 것들
-	//------------DEPTH 3
-	static IMAGE_LIST_BY_ALL = [...QueryKeys.IMAGE_INFLUENCED, 'LIST_BY_ALL']; //전체보기 이미지 목록
-	static IMAGE_LIST_BY_META_DATE = [...QueryKeys.IMAGE_INFLUENCED, 'LIST_BY_META_DATE']; //월별 이미지 목록
-	static IMAGE_LIST_BY_FOLDER = [...QueryKeys.IMAGE_INFLUENCED, 'LIST_BY_FOLDER']; //폴더별 이미지 목록
-	static IMAGE_LIST_BY_FAVORITE = [...QueryKeys.IMAGE_INFLUENCED, 'LIST_BY_FAVORITE']; //즐겨찾기 리스트
-	static IMAGE_LIST_BY_LOCATION = [...QueryKeys.IMAGE_INFLUENCED, 'LIST_BY_LOCATION']; //장소별 이미지 목록
-	static IMAGE_LIST_BY_UPLOADED = [...QueryKeys.IMAGE_INFLUENCED, 'LIST_BY_UPLOADED']; //업로드 이미지 목록
-	static IMAGE_LIST_BY_TRANSFER_FOLDER = [...QueryKeys.IMAGE_INFLUENCED, 'LIST_BY_TRANSFER_FOLDER']; //변경가능한 폴더의 이미지 목록
-	static ALARM_LIST = [...QueryKeys.IMAGE_INFLUENCED, 'ALARM_LIST']; //알림 리스트
-	static LOCATION_LIST = [...QueryKeys.IMAGE_INFLUENCED, 'LOCATION_LIST']; //장소 리스트
-	static SLIDE_THUMB = [...QueryKeys.IMAGE_INFLUENCED, 'SLIDE_THUMB']; //슬라이드 썸네일
-	static LATEST_FAVORITE = [...QueryKeys.IMAGE_INFLUENCED, 'LATEST_FAVORITE']; //최근 좋아요한 목록
-	static LATEST_LOCATION = [...QueryKeys.IMAGE_INFLUENCED, 'LATEST_LOCATION']; //최근 장소
-	static MAIN_DATE_LIST = [...QueryKeys.IMAGE_INFLUENCED, 'MAIN_DATE_LIST'];
-	static STORAGE_SIZE = [...QueryKeys.IMAGE_INFLUENCED, 'STORAGE_SIZE']; // 사용중인 총 이미지 용량
-	static FILTER_LIST = [...QueryKeys.IMAGE_INFLUENCED, 'FILTER_LIST']; //필터 집계정보
-	static FOLDER_LIST = [...QueryKeys.IMAGE_INFLUENCED, 'FOLDER_LIST']; //폴더 리스트
-
-	//가족 관련
-
-	static FAMILY_QUERY_GROUP = ['FAMILY_QUERY_GROUP'];
-
-	//아기 관련
-	static BABY_QUERY_GROUP = ['BABY_QUERY_GROUP'];
-
-	//클립 관련
-	static CLIP_QUERY_GROUP = ['CLIP_QUERY_GROUP'];
+	static TEST_QUERY = ['TEST_QUERY'];
 }
 
 export class QueryOptions {
